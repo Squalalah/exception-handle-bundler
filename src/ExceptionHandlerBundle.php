757eb693bef4b13b561101c3edbd520b7c66f22b
Anthony MONTREUIL <amontreuil@amiltone.com>
2022-02-10 15:03:35 +0100

    1.7.1 Correction du services.yaml, celui-ci est appelé automatiquement, plus besoin d'ajouter les 3 lignes.

--- src/ExceptionHandlerBundle.php
+++ src/ExceptionHandlerBundle.php
@@ -5,6 +5,9 @@ namespace Amontreuil\ExceptionHandlerBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
+//ce fichier peut paraitre inutile, mais il permet à symfony de detecter tout le dossier comme étant un "bundle"
+//permettant d'ajouter ce bundle au bundles.php du projet, et le services.yaml du bundle chargera donc correctement
+//Ce fichier est donc très important (comme chacun des fichiers du bundle)
 class ExceptionHandlerBundle extends Bundle
 {
 
